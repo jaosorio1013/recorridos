7887fb0916770489476311e6eafd280d432c5b25
Jhonatan Osorio <jaosorio1013@gmail.com>
2022-09-01 07:51:08 -0500

    first commit

---
+++ belleza-femenina/data.js
@@ -0,0 +1,127 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-belleza-femenina",
+      "name": "Belleza Femenina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.43348930101556427,
+        "pitch": 0.06967823108043802,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.124036656958083,
+          "pitch": 0.21986032296871016,
+          "rotation": 0,
+          "target": "1-lavacabezas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6114675384594843,
+          "pitch": -0.1661132348002532,
+          "title": "Herrajes",
+          "text": "Espejo con marco color dorado en pintura electrostática."
+        },
+        {
+          "yaw": -1.679141119282523,
+          "pitch": -0.15687322605360166,
+          "title": "Exhibidores",
+          "text": "Estanterías en poliuretano color blanco, con puertas de almacenamiento en la parte inferior."
+        }
+      ]
+    },
+    {
+      "id": "1-lavacabezas",
+      "name": "Lavacabezas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.23605654827574796,
+        "pitch": 0.027504905669236024,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.016928265558766142,
+          "pitch": 0.08420202898512485,
+          "rotation": 0,
+          "target": "2-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-recepcin",
+      "name": "Recepción",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.23169426592736286,
+        "pitch": -0.0025013618595046694,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BELLEZA FEMENINA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
